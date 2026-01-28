961e5f3f28b0d97079f54dc0878bea97df3027d5
shuaib <mohammedshuaibka@gmail.com>
2026-01-28 15:46:09 +0530

    new deplo

--- youway/settings.py
+++ youway/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^08+^%4wy0c+_hgx26^z51)zv!dep$i$q$+p-)q&ys%^0k0i_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
